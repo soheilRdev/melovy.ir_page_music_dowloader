7abc9a06c8463cd1c653d1a9fef7723f068f2076
SoheilRDeveloper <89535076+soheilRdev@users.noreply.github.com>
2022-07-17 11:37:25 +0430

    added two files

---
+++ main.py
@@ -0,0 +1,24 @@
+import requests
+import os
+from sys import platform
+from bs4 import BeautifulSoup
+
+get_link = input("Please enter your link from melovy.ir:\n ")
+
+url = get_link
+grab = requests.get(url)
+soup = BeautifulSoup(grab.text, 'html.parser')
+get_current_path = os.getcwd()
+# opening a file in write mode
+f = open("links.txt", "w")
+# traverse paragraphs from soup
+for link in soup.find_all("audio"):
+    data = link.get('src')
+    f.write(data)
+    f.write("\n")
+f.close()
+os.system("wget.exe -i links.txt")
+# if platform == "win32":
+#     os.system("cls")
+# else:
+#     os.system("clear")
