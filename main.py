cccdbbdb05158f40e58267c53a74aa090498f055
SoheilRDeveloper <89535076+soheilRdev@users.noreply.github.com>
2022-07-18 15:43:57 +0430

    remove ciomments and get_current_path = os.getcwd() from main.py

--- main.py
+++ main.py
@@ -8,10 +8,7 @@ get_link = input("Please enter your link from melovy.ir:\n ")
 url = get_link
 grab = requests.get(url)
 soup = BeautifulSoup(grab.text, 'html.parser')
-get_current_path = os.getcwd()
-# opening a file in write mode
 f = open("links.txt", "w")
-# traverse paragraphs from soup
 for link in soup.find_all("audio"):
     data = link.get('src')
     f.write(data)
