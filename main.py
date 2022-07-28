f2f02404261fa95d01ebea9146be20e8dbd2dd62
SoheilRDeveloper <89535076+soheilRdev@users.noreply.github.com>
2022-07-28 22:30:24 +0430

    made source code better to read

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
+from tracemalloc import start
 import requests
 import os
 from bs4 import BeautifulSoup
-
-get_link = input("Please enter your link from melovy.ir:\n ")
-
-url = get_link
-grab = requests.get(url)
-soup = BeautifulSoup(grab.text, 'html.parser')
-f = open("links.txt", "w")
-for link in soup.find_all("audio"):
-    data = link.get('src')
-    f.write(data)
-    f.write("\n")
-f.close()
-os.system("wget -i links.txt")
+class main():
+    def start():
+        get_link = input("Please enter your link from melovy.ir:\n ")
+        url = get_link
+        grab = requests.get(url)
+        soup = BeautifulSoup(grab.text, 'html.parser')
+        f = open("links.txt", "w")
+        for link in soup.find_all("audio"):
+            data = link.get('src')
+            f.write(data)
+            f.write("\n")
+        f.close()
+        os.system("wget -i links.txt")
+main.start()
